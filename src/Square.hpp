013ce659ef028cae7dd0c096856ae92f5f37145a
ay136416 <guo.sam@northeastern.edu>
2026-08-06 12:03:31 -0400

    feat: implement Square

---
+++ src/Square.hpp
@@ -0,0 +1,10 @@
+const int BOARD_SIZE = 8;
+struct Square {
+    int row, col;
+public:
+    Square() : Square(1,1);
+    Square(int r, int c) : row(r), col(c) {} 
+    bool isValid() {return (r>=1 && c>=1 && r<=BOARD_SIZE && c<=BOARD_SIZE)}
+    Square operator+(const Square& other) 
+    
+};
