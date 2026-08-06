21f0fa7ce35d51c477a631cae89f904cd464b730
ay136416 <guo.sam@northeastern.edu>
2026-08-06 12:49:49 -0400

    chore: put include guards in hpp files

--- src/Move.hpp
+++ src/Move.hpp
@@ -1,5 +1,8 @@
+#ifndef CS3520_MINIPROJECT_MOVE
+#define CS3520_MINIPROJECT_MOVE
 #include "Square.hpp"
 struct Move {
     Square startingSquare;
     Square endingSquare;
 };
+#endif
---
+++ src/Piece.hpp
@@ -0,0 +1,9 @@
+#include "Square.hpp"
+#include "Move.hpp"
+class Piece {
+    protected:
+        Square curSquare;
+        bool belongsToWhite;
+    public:
+
+}
