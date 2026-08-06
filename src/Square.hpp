ad310cd9e45a5d4a3a03e2861c9a36a474721454
ay136416 <guo.sam@northeastern.edu>
2026-08-06 12:27:23 -0400

    feat: implement Square::operator+

--- src/Square.hpp
+++ src/Square.hpp
@@ -5,6 +5,8 @@ public:
     Square() : Square(1,1);
     Square(int r, int c) : row(r), col(c) {} 
     bool isValid() {return (r>=1 && c>=1 && r<=BOARD_SIZE && c<=BOARD_SIZE)}
-    Square operator+(const Square& other) 
+    Square operator+(const Square& other) {
+        return Square(r + other.r, c+other.c);
+    }
     
 };
