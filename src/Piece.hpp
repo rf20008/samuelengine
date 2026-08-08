48534ad0856febfaada680c4e148ea4a8902a553
ay136416 <guo.sam@northeastern.edu>
2026-08-08 11:27:43 -0400

    chore: remove Square attribute from Piece

--- src/Piece.hpp
+++ src/Piece.hpp
@@ -9,17 +9,11 @@
 
 class Piece {
     protected:
-        Square curSquare;
         bool belongsToWhite;
     public:
-        Piece(const Square& square, bool ownedbyWhite) : curSquare(square), belongsToWhite(ownedbyWhite) {}
+        Piece(bool ownedbyWhite) : belongsToWhite(ownedbyWhite) {}
         bool belongsToWhite() const final {return belongsToWhite;}
         virtual char symbol() const = 0;
-        Square getSquare() const {return this->curSquare;}
-        virtual std::set<Square> possibleMoves() const = 0;
-        void setSquare(Square newSquare) {
-            curSquare=newSquare;
-        }
 };
 
 #endif
