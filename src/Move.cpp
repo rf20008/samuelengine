96f962082173561505a9e4c11d5e3cd81198d02e
ay136416 <guo.sam@northeastern.edu>
2026-08-08 17:36:13 -0400

    chore: make Move::operator< const

--- src/Move.cpp
+++ src/Move.cpp
@@ -1,7 +1,7 @@
 #include "Move.hpp"
 #include "Errors.hpp"
 
-bool Move::operator<(const Move& other) {
+bool Move::operator<(const Move& other) const {
     // to be done by Joshua. Please create a total ordering of Move, and return whether, in that ordering, this is considered less than other
 
     throw NotImplementedError("Move::operator<(const Move& other) is not implemented yet");
--- src/Move.hpp
+++ src/Move.hpp
@@ -4,6 +4,6 @@
 struct Move {
     Square startingSquare;
     Square endingSquare;
-    bool operator<(const Move& other); // to be done by Joshua
+    bool operator<(const Move& other) const; // to be done by Joshua
 };
 #endif
