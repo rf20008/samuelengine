1538095c7e0ee5b7b17eaf71c4d0e690c7b34b5b
ay136416 <guo.sam@northeastern.edu>
2026-08-08 11:49:44 -0400

    fix: fix syntax errors in Square.hpp

--- src/Square.hpp
+++ src/Square.hpp
@@ -4,9 +4,9 @@ const int BOARD_SIZE = 8;
 struct Square {
     int row, col;
 public:
-    Square() : Square(1,1);
+    Square() : Square(1,1) {}
     Square(int r, int c) : row(r), col(c) {} 
-    bool isValid() {return (row>=1 && col>=1 && row<=BOARD_SIZE && col<=BOARD_SIZE)}
+    bool isValid() {return (row>=1 && col>=1 && row<=BOARD_SIZE && col<=BOARD_SIZE);}
     Square operator+(const Square& other) {
         return Square(row + other.row, col+other.col);
     }
