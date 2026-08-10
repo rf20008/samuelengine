da563640b3e9f744470e241d70cab49e40c6d4b5
duckmaster <guo.jos@northeastern.edu>
2026-08-10 01:48:51 -0400

    feat: implement Move::operator<

--- src/Move.cpp
+++ src/Move.cpp
@@ -2,7 +2,8 @@
 #include "Errors.hpp"
 
 bool Move::operator<(const Move& other) const {
-    // to be done by Joshua. Please create a total ordering of Move, and return whether, in that ordering, this is considered less than other
-
-    throw NotImplementedError("Move::operator<(const Move& other) is not implemented yet");
+	if (this->startingSquare != other.startingSquare){
+		return (this->startingSquare < other.startingSquare);
+	}
+	return (this->endingSquare < other.endingSquare);
 }
