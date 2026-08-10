3107ffcb6664efe5a5d11c85fe640e48c7ccf5b6
duckmaster <guo.jos@northeastern.edu>
2026-08-10 00:56:51 -0400

    test: test ChessBoard member constructor in test/Board.cxxtest.hpp

--- test/Board.cxxtest.hpp
+++ test/Board.cxxtest.hpp
@@ -4,7 +4,24 @@
 class TestBoard: public CxxTest::TestSuite
 {
 	public:
-		void testSanityCheck(){
-			TS_ASSERT_EQUALS(1 + 1, 2);
+		void testChessBoardMemberConstructor(){
+			std::vector<std::vector<std::shared_ptr<Piece>>> pieces = {};
+			bool whiteToMove = true;
+			PlayerState whitePlayerState = {true, true};
+			PlayerState blackPlayerState = {true, true};
+			int halfmove_clock = 0;
+			int fullmove_clock = 0;
+			Square enPassant_targetSquare = { -1, -1 };
+
+			ChessBoard myBoard = ChessBoard(
+				pieces,
+				whiteToMove,
+				whitePlayerState,
+				blackPlayerState,
+				halfmove_clock,
+				fullmove_clock,
+				enPassant_targetSquare
+			);
+			(void)myBoard;
 		}
 };
