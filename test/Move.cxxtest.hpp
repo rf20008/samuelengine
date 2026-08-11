67dd143a7e5c4e5bc110f17f322aa10f88851f74
duckmaster <guo.jos@northeastern.edu>
2026-08-11 00:28:18 -0400

    style: reformat all of test/

--- test/Move.cxxtest.hpp
+++ test/Move.cxxtest.hpp
@@ -1,17 +1,17 @@
-#include <cxxtest/TestSuite.h>
 #include <Move.hpp>
+#include <cxxtest/TestSuite.h>
 
-class TestMove: public CxxTest::TestSuite{
+class TestMove : public CxxTest::TestSuite {
 	public:
-		void testMoveConstructor(){
-			Square start = { 1, 2 };
-			Square end = { 4, 8 };
-			Move m = { start, end };
+		void testMoveConstructor() {
+			Square start = {1, 2};
+			Square end = {4, 8};
+			Move m = {start, end};
 			TS_ASSERT_EQUALS(m.startingSquare, start);
 			TS_ASSERT_EQUALS(m.endingSquare, end);
 		}
 
-		void testMoveComparison(){
+		void testMoveComparison() {
 			// chosen with the help of an RNG (you can change this if you wish)
 			Move m0 = {{8, 9}, {0, 5}};
 			Move m00 = {{0, 5}, {4, 2}};
