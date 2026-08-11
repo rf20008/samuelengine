1532ef1ef796fdeea816ea46f30817229408ffaa
ay136416 <guo.sam@northeastern.edu>
2026-08-11 12:49:48 -0400

    test(FEN): test parsePlayerPart

---
+++ test/FEN.cxxtest.hpp
@@ -0,0 +1,14 @@
+#include "FEN.hpp"
+
+#include "cxxtest/TestSuite.h"
+using namespace ParsePieces;
+
+class TestFEN : public CxxTest::TestSuite {
+    void testPlayerGet() {
+        TS_ASSERT_EQUALS(true, ParsePieces::parsePlayerPart("w"));
+        TS_ASSERT_EQUALS(false, ParsePieces::parsePlayerPart("b"));
+        TS_ASSERT_THROWS_ANYTHING(ParsePieces::parsePlayerPart(""));
+        TS_ASSERT_THROWS_ANYTHING(parsePlayerPart("t"));
+        TS_ASSERT_THROWS_ANYTHING(parsePlayerPart("ttt"));
+    }
+}
