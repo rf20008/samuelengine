bba5668e145facb56a24f29b670be0aaa07e3f88
ay136416 <guo.sam@northeastern.edu>
2026-08-11 13:01:45 -0400

    test(FEN): test getCastlingPart

--- test/FEN.cxxtest.hpp
+++ test/FEN.cxxtest.hpp
@@ -18,4 +18,10 @@ class TestFEN : public CxxTest::TestSuite {
             TS_ASSERT_EQUALS(getPlayerPart(true), "w");
             TS_ASSERT_EQUALS(getPlayerPart(false), "b");
         }
+        void testGetCastlingPart() {
+            TS_ASSERT_EQUALS(getCastlingPart("KQkq"), {PlayerState(true, true), PlayerState(true, true)});
+            TS_ASSERT_EQUALS(getCastlingPart("-"), {PlayerState(false, false), PlayerState{false, false}});
+            TS_ASSERT_EQUALS(getCastlingPart("KQ"), {PlayerState(true, true), PlayerState(false, false)});
+            TS_ASSERT_EQUALS(getCastlingPart("Kk"), {PlayerState(false, true), PlayerState(false, true)});
+        }
 };
