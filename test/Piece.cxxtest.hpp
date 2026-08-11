9de4ff18225265ac89f4e8e23a5e8a4e3a55cbd9
duckmaster <guo.jos@northeastern.edu>
2026-08-10 23:57:26 -0400

    style: standardize indentation in test/Piece.cxxtest.hpp

--- test/Piece.cxxtest.hpp
+++ test/Piece.cxxtest.hpp
@@ -20,53 +20,53 @@ class TestPiece: public CxxTest::TestSuite{
 			TS_ASSERT_EQUALS(b.symbol(), 'b');
 		}
 		void testWhiteKing(){
-                        King k = King(true);
-                        TS_ASSERT_EQUALS(k.getBelongsToWhite(), true);
-                        TS_ASSERT_EQUALS(k.symbol(), 'K');
-                }
-                void testBlackKing(){
-                        King k = King(false);
-                        TS_ASSERT_EQUALS(k.getBelongsToWhite(), false);
-                        TS_ASSERT_EQUALS(k.symbol(), 'k');
-                }
+			King k = King(true);
+			TS_ASSERT_EQUALS(k.getBelongsToWhite(), true);
+			TS_ASSERT_EQUALS(k.symbol(), 'K');
+		}
+		void testBlackKing(){
+			King k = King(false);
+			TS_ASSERT_EQUALS(k.getBelongsToWhite(), false);
+			TS_ASSERT_EQUALS(k.symbol(), 'k');
+		}
 		void testWhiteKnight(){
-                        Knight n = Knight(true);
-                        TS_ASSERT_EQUALS(n.getBelongsToWhite(), true);
-                        TS_ASSERT_EQUALS(n.symbol(), 'N');
-                }
-                void testBlackKnight(){
-                        Knight n = Knight(false);
-                        TS_ASSERT_EQUALS(n.getBelongsToWhite(), false);
-                        TS_ASSERT_EQUALS(n.symbol(), 'n');
-                }
+			Knight n = Knight(true);
+			TS_ASSERT_EQUALS(n.getBelongsToWhite(), true);
+			TS_ASSERT_EQUALS(n.symbol(), 'N');
+		}
+		void testBlackKnight(){
+			Knight n = Knight(false);
+			TS_ASSERT_EQUALS(n.getBelongsToWhite(), false);
+			TS_ASSERT_EQUALS(n.symbol(), 'n');
+		}
 		void testWhitePawn(){
-                        Pawn p = Pawn(true);
-                        TS_ASSERT_EQUALS(p.getBelongsToWhite(), true);
-                        TS_ASSERT_EQUALS(p.symbol(), 'P');
-                }
-                void testBlackPawn(){
-                        Pawn p = Pawn(false);
-                        TS_ASSERT_EQUALS(p.getBelongsToWhite(), false);
-                        TS_ASSERT_EQUALS(p.symbol(), 'p');
-                }
+			Pawn p = Pawn(true);
+			TS_ASSERT_EQUALS(p.getBelongsToWhite(), true);
+			TS_ASSERT_EQUALS(p.symbol(), 'P');
+		}
+		void testBlackPawn(){
+			Pawn p = Pawn(false);
+			TS_ASSERT_EQUALS(p.getBelongsToWhite(), false);
+			TS_ASSERT_EQUALS(p.symbol(), 'p');
+		}
 		void testWhiteQueen(){
-                        Queen q = Queen(true);
-                        TS_ASSERT_EQUALS(q.getBelongsToWhite(), true);
-                        TS_ASSERT_EQUALS(q.symbol(), 'Q');
-                }
-                void testBlackQueen(){
-                        Queen q = Queen(false);
-                        TS_ASSERT_EQUALS(q.getBelongsToWhite(), false);
-                        TS_ASSERT_EQUALS(q.symbol(), 'q');
-                }
+			Queen q = Queen(true);
+			TS_ASSERT_EQUALS(q.getBelongsToWhite(), true);
+			TS_ASSERT_EQUALS(q.symbol(), 'Q');
+		}
+		void testBlackQueen(){
+			Queen q = Queen(false);
+			TS_ASSERT_EQUALS(q.getBelongsToWhite(), false);
+			TS_ASSERT_EQUALS(q.symbol(), 'q');
+		}
 		void testWhiteRook(){
-                        Rook r = Rook(true);
-                        TS_ASSERT_EQUALS(r.getBelongsToWhite(), true);
-                        TS_ASSERT_EQUALS(r.symbol(), 'R');
-                }
-                void testBlackRook(){
-                        Rook r = Rook(false);
-                        TS_ASSERT_EQUALS(r.getBelongsToWhite(), false);
-                        TS_ASSERT_EQUALS(r.symbol(), 'r');
-                }
+			Rook r = Rook(true);
+			TS_ASSERT_EQUALS(r.getBelongsToWhite(), true);
+			TS_ASSERT_EQUALS(r.symbol(), 'R');
+		}
+		void testBlackRook(){
+			Rook r = Rook(false);
+			TS_ASSERT_EQUALS(r.getBelongsToWhite(), false);
+			TS_ASSERT_EQUALS(r.symbol(), 'r');
+		}
 };
