8c8b9313d13df99d6d227dbf40bd47ae98f6fc1b
ay136416 <guo.sam@northeastern.edu>
2026-08-12 11:26:02 -0400

    test(ChessBoard): test white kingside castling and black kingside castling

--- test/ChessBoard.cxxtest.hpp
+++ test/ChessBoard.cxxtest.hpp
@@ -85,7 +85,7 @@ class TestBoard : public CxxTest::TestSuite {
             TS_ASSERT_EQUALS(myBoard.perft(2), 191);
             TS_ASSERT_EQUALS(myBoard.perft(3), 2039);
         }
-        void testKingsideCastling() {
+        void testWhiteKingsideCastling() {
             ChessBoard myBoard ("6k1/8/8/8/8/8/8/4K2R w K - 0 1");
             TS_ASSERT_EQUALS(myBoard.findKing(true), "e1");
             TS_ASSERT(myBoard.isMoveLegal(Move("e1", "g1")));
@@ -101,8 +101,8 @@ class TestBoard : public CxxTest::TestSuite {
                 TS_FAIL("f1 does not contain rook");
             }
         }
-        void testQueensideCastling() {
-            ChessBoard myBoard ("6k1/8/8/8/8/8/8/R3K3 w K - 0 1");
+        void testWhiteQueensideCastling() {
+            ChessBoard myBoard ("6k1/8/8/8/8/8/8/R3K3 w Q - 0 1");
             TS_ASSERT(myBoard.isMoveLegal(Move("e1", "c1")));
             myBoard.processMove(Move("e1", "c1"));
             try {
@@ -114,6 +114,37 @@ class TestBoard : public CxxTest::TestSuite {
                 TS_ASSERT(myBoard.getAndAssertPiece("d1", 'R'));
             } catch (WrongPieceType& err) {
                 TS_FAIL("f1 does not contain rook");
+            }
+        }
+        void testBlackKingsideCastling() {
+            ChessBoard myBoard ("4k2R/8/8/8/8/8/8/6K1 b k - 0 1");
+            TS_ASSERT_EQUALS(myBoard.findKing(false), "e8");
+            TS_ASSERT(myBoard.isMoveLegal(Move("e8", "g8")));
+            myBoard.processMove(Move("e8", "g8"));
+            try {
+                TS_ASSERT(myBoard.getAndAssertPiece("g8", 'K'));
+            } catch (WrongPieceType& err) {
+                TS_FAIL("g8 does not contain king");
+            }
+            try {
+                TS_ASSERT(myBoard.getAndAssertPiece("f8", 'R'));
+            } catch (WrongPieceType& err) {
+                TS_FAIL("f8 does not contain rook");
+            }
+        }
+        void testBlackQueensideCastling() {
+            ChessBoard myBoard ("r3k3/8/8/8/8/8/8/6K1 b q - 0 1");
+            TS_ASSERT(myBoard.isMoveLegal(Move("e8", "c8")));
+            myBoard.processMove(Move("e8", "c8"));
+            try {
+                TS_ASSERT(myBoard.getAndAssertPiece("c8", 'K'));
+            } catch (WrongPieceType& err) {
+                TS_FAIL("g8 does not contain king");
+            }
+            try {
+                TS_ASSERT(myBoard.getAndAssertPiece("d8", 'R'));
+            } catch (WrongPieceType& err) {
+                TS_FAIL("d8 does not contain rook");
             }
         }
 
