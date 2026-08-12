c4d99eaf1727b2308c3a115c07b12e341ec18d24
duckmaster <guo.jos@northeastern.edu>
2026-08-12 16:08:14 -0400

    feat: use promotion attribute in Move::operator<

--- src/Move.cpp
+++ src/Move.cpp
@@ -5,5 +5,8 @@ bool Move::operator<(const Move &other) const {
 	if (this->startingSquare != other.startingSquare) {
 		return (this->startingSquare < other.startingSquare);
 	}
-	return (this->endingSquare < other.endingSquare);
+	if (this->endingSquare != other.endingSquare) {
+		return (this->endingSquare < other.endingSquare);
+	}
+	return (this->promotion < other.promotion);
 }
