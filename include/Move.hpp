b6ff4af861e7dad6268365c3a9f4b7713cd1b897
ay136416 <guo.sam@northeastern.edu>
2026-08-12 16:44:55 -0400

    feat(Move): implement default constructor

--- include/Move.hpp
+++ include/Move.hpp
@@ -5,6 +5,13 @@ struct Move {
 		Square startingSquare;
 		Square endingSquare;
 		char promotion = '\0';
+        Move() : startingSquare("a1"), endingSquare("a1"), promotion('\0') {}
+        Move(const Square start, const Square end, const char promo='\0') : startingSquare(start), endingSquare(end), promotion(promo) {}
+        Move(const Move& other) = default;
+        Move(Move&& other) = default;
+        Move& operator=(const Move& other) = default;
+        Move& operator=(Move&& other) = default;
+        ~Move() = default;
 		bool operator<(const Move &other) const;
 		bool operator==(const Move &other) const;
 		std::string operator()() const; 
