83fec217f8409502a0e05b19ab45a9e9a810558f
ay136416 <guo.sam@northeastern.edu>
2026-08-28 19:32:26 -0400

    fix(testChessBoard): correctly copy perft results from https://chessprogramming.org/Perft_Results in position 6

--- test/ChessBoard.cxxtest.hpp
+++ test/ChessBoard.cxxtest.hpp
@@ -113,9 +113,10 @@ class TestBoard : public CxxTest::TestSuite {
         void testPerftPositionSeven() {
             ChessBoard myBoard("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
             TS_ASSERT_EQUALS(myBoard.get_whiteToMove(), true);
-            TS_ASSERT_EQUALS(myBoard.perft(1), 1);
-            TS_ASSERT_EQUALS(myBoard.perft(2), 46);
-            TS_ASSERT_EQUALS(myBoard.perft(3), 2079);
+            TS_ASSERT_EQUALS(myBoard.perft(0), 1);
+            TS_ASSERT_EQUALS(myBoard.perft(1), 46);
+            TS_ASSERT_EQUALS(myBoard.perft(2), 2079);
+            TS_ASSERT_EQUALS(myBoard.perft(3), 89890);
         }
 		void testWhiteKingsideCastling() {
 			ChessBoard myBoard("6k1/8/8/8/8/8/8/4K2R w K - 0 1");
