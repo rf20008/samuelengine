b472de0b07c226e1616eb7dbaa9972a085df6559
ay136416 <guo.sam@northeastern.edu>
2026-08-28 19:17:53 -0400

    test(boardDisplay): test displaying the standard position

---
+++ test/boardDisplay.cxxtest.hpp
@@ -0,0 +1,19 @@
+
+#include "board-display.hpp"
+#include <cxxtest/TestSuite.h>
+#include <iostream>
+class TestBoardDisplay : public CxxTest::TestSuite {
+public:
+    void testDisplayBasicBoard() {
+        TS_ASSERT_EQUALS(ChessUI::renderBoard(ChessBoard()),
+            "8 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖\n"
+            "7 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙\n"
+            "6 . . . . . . . .\n"
+            "5 . . . . . . . .\n"
+            "4 . . . . . . . .\n"
+            "3 . . . . . . . .\n"
+            "2 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟\n"
+            "1 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜\n"
+            "  a b c d e f g h\n");
+    }
+};
