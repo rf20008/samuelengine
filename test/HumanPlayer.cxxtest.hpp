3d9a030da45a392317e59c94023e3b7ecdfc1c8b
duckmaster <duckmaster0@protonmail.com>
2026-08-28 19:55:34 -0400

    style: refactor test/HumanPlayer.cxxtest.hpp using clang-format

--- test/HumanPlayer.cxxtest.hpp
+++ test/HumanPlayer.cxxtest.hpp
@@ -3,9 +3,9 @@
 
 class TestHumanPlayer : public CxxTest::TestSuite {
 	public:
-		void testTryParseMoveInvalid(){
+		void testTryParseMoveInvalid() {
 			// empty string
-			const Move myMove = {{1, 2}, {3, 4}}; // arbitrary; feel free to change this 
+			const Move myMove = {{1, 2}, {3, 4}}; // arbitrary; feel free to change this
 			Move m = myMove;
 			TS_ASSERT_EQUALS(tryParseMove("", m), false);
 			TS_ASSERT_EQUALS(m, myMove);
@@ -14,18 +14,18 @@ class TestHumanPlayer : public CxxTest::TestSuite {
 			m = myMove;
 			TS_ASSERT_EQUALS(tryParseMove("hi", m), false);
 			TS_ASSERT_EQUALS(m, myMove);
-			
+
 			// nonempty, too long
 			m = myMove;
 			TS_ASSERT_EQUALS(tryParseMove("lorem ipsum dolor sit amet consectetur adipiscing elit", m), false);
 			TS_ASSERT_EQUALS(m, myMove);
 
 			// fileFrom too low
-			
+
 			// fileFrom too high
 
 			// fileTo too low
-			
+
 			// fileTo too high
 
 			// rankFrom too low
