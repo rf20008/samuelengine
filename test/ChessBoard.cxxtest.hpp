0d2c7bd34f586e54854e6a6e906616e45d745cd0
ay136416 <guo.sam@northeastern.edu>
2026-08-29 17:22:19 -0400

    fix(perft): rename perft undo tests to not collide with perft other tests

--- test/ChessBoard.cxxtest.hpp
+++ test/ChessBoard.cxxtest.hpp
@@ -139,14 +139,14 @@ class TestBoard : public CxxTest::TestSuite {
 			TS_ASSERT_EQUALS(myBoard.perft(2), myBoard.perftCopy(2));
 			TS_ASSERT_EQUALS(myBoard.perft(3), myBoard.perftCopy(3));
 		}
-		void testPerfPosition2() {
+		void testUndoPerfPosition2() {
 			// credit to https://chessprogramming.org/Perft_Results for providing the position and correct perft results
 			ChessBoard myBoard = ChessBoard("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 			// will only work when castling is implemented
 			TS_ASSERT_EQUALS(myBoard.perft(1), myBoard.perftCopy(1));
 			TS_ASSERT_EQUALS(myBoard.perft(2), myBoard.perftCopy(2));
 		}
-		void testPerftPositionThree() {
+		void testUndoPerftPositionThree() {
 			// also from PerftResults
 			ChessBoard myBoard("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1");
             TS_ASSERT_EQUALS(myBoard.perft(1), myBoard.perftCopy(1));
@@ -154,7 +154,7 @@ class TestBoard : public CxxTest::TestSuite {
 			TS_ASSERT_EQUALS(myBoard.perft(3), myBoard.perftCopy(3));
 		}
 
-		void testPerftPositionFour() {
+		void testUndoPerftPositionFour() {
 			ChessBoard myBoard("8/P6k/8/8/8/8/8/7K w - - 0 1");
 			TS_ASSERT_EQUALS(myBoard.perft(1), myBoard.perftCopy(1));
 			TS_ASSERT_EQUALS(myBoard.perft(2), myBoard.perftCopy(2));
@@ -162,19 +162,19 @@ class TestBoard : public CxxTest::TestSuite {
             TS_ASSERT_EQUALS(myBoard.perft(4), myBoard.perftCopy(4));
 			
 		}
-		void testPerftPositionFive() {
+		void testUndoPerftPositionFive() {
 			ChessBoard myBoard("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
 			TS_ASSERT_EQUALS(myBoard.perft(1), myBoard.perftCopy(1));
 			TS_ASSERT_EQUALS(myBoard.perft(2), myBoard.perftCopy(2));
 			TS_ASSERT_EQUALS(myBoard.perft(3), myBoard.perftCopy(3));
 		}
 		
-		void testPerftPositionSix() {
+		void testUndoPerftPositionSix() {
 			ChessBoard myBoard("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
 			TS_ASSERT_EQUALS(myBoard.perft(1), myBoard.perftCopy(1));
 			TS_ASSERT_EQUALS(myBoard.perft(2), myBoard.perftCopy(2));
 		}
-		void testPerftPositionSeven() {
+		void testUndoPerftPositionSeven() {
 			ChessBoard myBoard("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 			TS_ASSERT_EQUALS(myBoard.get_whiteToMove(), true);
 			TS_ASSERT_EQUALS(myBoard.perft(0), myBoard.perftCopy(0));
