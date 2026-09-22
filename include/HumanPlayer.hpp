092da71067e22a8e283e71f5226118e8afcb56b2
duckmaster <duckmaster0@protonmail.com>
2026-09-21 20:12:07 -0400

    fix: update tryParseMove declaration and tests to use new type signature with a board

--- include/HumanPlayer.hpp
+++ include/HumanPlayer.hpp
@@ -1,8 +1,9 @@
 #ifndef CS3520_MINIPROJECT_HUMANPLAYER
 #define CS3520_MINIPROJECT_HUMANPLAYER
 #include "AbstractPlayer.hpp"
+#include "ChessBoard.hpp"
 
-bool tryParseMove(const std::string &raw, Move &out);
+bool tryParseMove(ChessBoard &board, const std::string &raw, Move &out);
 
 class HumanPlayer : public AbstractPlayer {
 	public:
